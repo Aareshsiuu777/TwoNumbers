39724fd511ea9c50fb2b6a219da389e5322251e3
Aareshsiuu777 <maharjah@mail.uc.edu>
2026-03-12 22:16:45 -0400

    Initial Push

--- src/TwoNumbers.java
+++ src/TwoNumbers.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class TwoNumbers {
+
+    public static int twoNumbers(int num1, int num2, int num3) {
+        int sum = num1 + num2;
+        int result = sum * num3;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int answer1 = twoNumbers(8, 4, 5);
+        System.out.println("Test 1: (8 + 4) * 5 = " + answer1);
+
+        int answer2 = twoNumbers(3, 7, 2);
+        System.out.println("Test 2: (3 + 7) * 2 = " + answer2);
+
+        int answer3 = twoNumbers(10, 5, 3);
+        System.out.println("Test 3: (10 + 5) * 3 = " + answer3);
+    }
 }
